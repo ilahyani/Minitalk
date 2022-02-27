21571498e1c9b2cacc2afbf40de1bcd7cb76c7d2
ismail lahyani <lahyani799@gmail.com>
2022-02-27 04:13:05 +0100

    client works

--- client.c
+++ client.c
@@ -1,9 +1,37 @@
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int ac, char **av)
 {
-	kill(atoi(av[1]), SIGUSR1);
-//	kill(atoi(av[1]), SIGUSR2);
-//	kill(atoi(av[1]), SIGINT);
+	int	i;
+	int	j;
+	int	z;
+
+	if (ac != 3)
+		return (0);
+	j = 0;
+	while (j < strlen(av[2]))
+	{
+		z = 128;
+		i = 1;
+		while (i < 9)
+		{
+			if ((z & av[2][j]) == 0)
+			{
+				printf ("0");
+				kill(atoi(av[1]), SIGUSR1);
+			}
+			else if ((z & av[2][j]) == z)
+			{
+				printf ("1");
+				kill(atoi(av[1]), SIGUSR2);
+			}
+			z = z >> 1;
+			i++;
+		}
+		j++;
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	handlerr(int signum)
+void	handler(int signum)
 {
 	if (signum == SIGUSR1)
-		write(1, "1\n", 2);
+		write(1, "0", 1);
 	else
-		write(1, "0\n", 2);
+		write(1, "1", 1);
 }
 int main()
 {
 	struct sigaction sa = {0};
 
-	sa.sa_handler = &handlerr;
+	sa.sa_handler = &handler;
 	printf("%d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	while (1);
+	while (1)
+		pause();
 	return (0);
 }
