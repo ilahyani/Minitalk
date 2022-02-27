ae06a34148ac9e5712c7baf6fb0f14be110373ec
Ismail Lahyani <ilahyani@e2r9p11.1337.ma>
2022-02-27 21:48:33 +0100

    client & server work

--- server.c
+++ server.c
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilahyani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 19:15:41 by ilahyani          #+#    #+#             */
+/*   Updated: 2022/02/27 21:21:23 by ilahyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
+int j = 7;
+/*
 void	handler(int signum)
 {
 	if (signum == SIGUSR1)
@@ -9,14 +24,29 @@ void	handler(int signum)
 	else
 		write(1, "1", 1);
 }
+
+*/
+void	handler(int signum)
+{
+	static int	byte = 0;
+
+	if (signum == SIGUSR2)
+		byte += pow(2, j);
+//	printf("-->%c\n", byte);
+	if (--j == -1)
+	{
+		write(1, &byte, 1);
+		byte = 0;
+		j = 7;
+	}
+//	printf("-->%d\n", j);
+}
+
 int main()
 {
-	struct sigaction sa = {0};
-
-	sa.sa_handler = &handler;
 	printf("%d\n", getpid());
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
 	while (1)
 		pause();
 	return (0);
