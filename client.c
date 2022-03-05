ec020a25cf0d83ff89c80aabda84e59e2020f86d
Ismail Lahyani <ilahyani@e2r9p11.1337.ma>
2022-03-05 18:50:32 +0100

    Server & Client work

--- client.c
+++ client.c
@@ -3,37 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilahyani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 21:02:58 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/03/01 09:45:57 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/03/05 18:48:57 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+
+void	handler(int signum)
+{
+	printf("Your Message Has Been Received Successfully\n");
+}
 
 int main(int ac, char **av)
 {
 	int	i;
 	int	j;
 	int	z;
+	size_t size;
 
-	if (ac != 3)
+	if (ac != 3 || av[1] <= 1)
 		return (0);
+	signal(SIGUSR1, handler);
 	j = 0;
-	while (av[2][j])
+	size = strlen (av[2]);
+	while (j <= size)
 	{
 		z = 128;
 		i = 1;
 		while (i < 9)
 		{
 			if ((z & av[2][j]) == 0)
-				kill(atoi(av[1]), SIGUSR1);
+				kill(atoi(av[1]), SIGUSR1); //error if pid is got other chars than digits 
 			else if ((z & av[2][j]) == z)
-				kill(atoi(av[1]), SIGUSR2);
+				kill(atoi(av[1]), SIGUSR2); // ft_atoi
 			z = z >> 1;
 			i++;
 			usleep(500);
--- server.c
+++ server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilahyani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 19:15:41 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/03/01 10:24:23 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/03/05 18:48:20 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,28 @@
 
 int j = 7;
 
-void	handler(int signum)
+void	handler(int signum, siginfo_t *info, void *context)
 {
 	static int	byte = 0;
+	static int og_pid;
 
-/*	if (signum != SIGUSR1 && signum != SIGUSR2)
+	if (!og_pid)
+		og_pid = info->si_pid;
+	if (og_pid != info->si_pid && j != 7)
 	{
-		j = 7;
+		og_pid = 0;
 		byte = 0;
-	}*/
+		j = 7;
+	}
 	if (signum == SIGUSR2)
 		byte += pow(2, j); //ft_pow
 	if (--j == -1)
 	{
-		write(1, &byte, 1);
+		if (byte == 0)
+			kill(info->si_pid, SIGUSR1);
+		else
+			write(1, &byte, 1);
+		og_pid = 0;
 		byte = 0;
 		j = 7;
 	}
@@ -39,10 +47,13 @@ void	handler(int signum)
 
 int main()
 {
-	printf("%d\n", getpid()); //error if pid is got other chars than digits 
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
-	//signal(SIGINT, handler);
+	struct sigaction sa;
+
+	sa.sa_sigaction = &handler;
+	sa.sa_flags = SA_SIGINFO;
+	printf("%d\n", getpid()); //ft_printf
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
